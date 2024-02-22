f87012c013bf9c9b34d0350b334167f65bc47a86
ashiqLSD <asrafulas242@gmail.com>
2024-02-22 17:31:57 +0600

    first commit

---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype {
+
+    public static void main(String[] args) {
+        boolean b;
+        char c;
+        short s=3068;//dynamic initialization
+        int i=2203068;
+        float f=4.44f;
+        double d=20.34;
+        b= true;
+        System.out.println("b="+b);
+        c= 'r';
+        System.out.println("c="+c);
+        System.out.println("s="+s);
+        System.out.println("i="+i);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+    }
+
+}
